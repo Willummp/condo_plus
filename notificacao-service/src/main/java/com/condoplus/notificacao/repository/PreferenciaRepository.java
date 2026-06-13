766e90bd4bcdcdd35ddc5d1537b1bbbea9efbb11
Rodolpho Rodrigues <rodolphoeta@gmail.com>
2026-06-13 06:28:29 -0300

    Adicionei as interfaces de notificacao e preferencia do repository

--- notificacao-service/src/main/java/com/condoplus/notificacao/repository/PreferenciaRepository.java
+++ notificacao-service/src/main/java/com/condoplus/notificacao/repository/PreferenciaRepository.java
@@ -1,4 +1,17 @@
 package com.condoplus.notificacao.repository;
+import com.condoplus.notificacao.domain.PreferenciaNotificacao;
+import com.condoplus.notificacao.domain.TipoEvento;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import java.util.UUID;
 
-public interface PreferenciaRepository {
+public interface PreferenciaRepository extends ReactiveCrudRepository<PreferenciaNotificacao, UUID> {
+    Flux<PreferenciaNotificacao> findByPessoaIdAndTipoEventoAndAtivaTrue(
+            UUID pessoaId, TipoEvento tipoEvento);
+
+    Flux<PreferenciaNotificacao> findByPessoaId(UUID pessoaId);
+    Mono<PreferenciaNotificacao> findByPessoaIdAndTipoEventoAndCanal(
+            UUID pessoaId, TipoEvento tipoEvento,
+            com.condoplus.notificacao.domain.Canal canal);
 }
