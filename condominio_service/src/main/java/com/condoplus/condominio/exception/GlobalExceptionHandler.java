19dedf21d0dc3a93dd7a9df47e501dae6cc8da1b
Willummp <lsousaah@gmail.com>
2026-06-21 16:14:21 -0300

    fix: seed admin via Flyway V2 e corrigir AuthorizationDeniedException retornando 403 no IAM e condominio

--- condominio_service/src/main/java/com/condoplus/condominio/exception/GlobalExceptionHandler.java
+++ condominio_service/src/main/java/com/condoplus/condominio/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -92,6 +93,11 @@ public class GlobalExceptionHandler {
         p.setType(URI.create("https://condoplus.local/errors/validacao"));
         p.setProperty("erros", erros);
         return ResponseEntity.badRequest().body(p);
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ProblemDetail> handleAcessoNegado(AuthorizationDeniedException ex) {
+        return problem(HttpStatus.FORBIDDEN, "Acesso negado", "Acesso negado", "/errors/acesso-negado");
     }
 
     @ExceptionHandler(Exception.class)
